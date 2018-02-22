6a66cacd8f913f19edb896105f231239d591a7e7
James Sakurai <jamessakurai@Jamess-MacBook-Air.local>
2018-02-22 16:13:55 -0700

    Refactored Square into a Component

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+// import React, { Component } from 'react';
+
+
 
 class Board extends React.Component {
   renderSquare(i) {
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+// import React, { Component } from 'react';
+
 
 class Game extends React.Component {
   constructor(props) {
